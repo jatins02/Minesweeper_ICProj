771f95dfd75a0ad148da1a9327ec93a9b551457c
Jatin Singh <jatinsingh6654@gmail.com>
2026-03-23 19:44:59 +0530

    cleaned up code, removed unnecessary comments, added useful ones

--- TrialSFML.cpp
+++ TrialSFML.cpp
@@ -2,12 +2,10 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <ctime>
-#include <vector>
 
 const unsigned int WIDTH = 560;
 const unsigned int HEIGHT = 690;
 
-typedef enum { easy, mid, hard, none } difficulties;
 
 class DifficultyBtn : public sf::RectangleShape {
 private:
@@ -27,7 +25,7 @@ public:
 
 
 	DifficultyBtn(std::string text, sf::Font &font, float yPos)			// get an error saying, no default constructor for the sf::Text class, this is because, you have to pass in the font to the text class as soon as the constructor is called.
-		: content(font, text, 22){					// you can pass in the text string as well as the size of characters while initialising sf::Text
+		: content(font, text, 22){										// you can pass in the text string as well as the size of characters while initialising sf::Text
 
 		this->setSize(sf::Vector2f(BTN_WIDTH, BTN_HEIGHT));
 		this->setOrigin(this->getGeometricCenter());
@@ -44,7 +42,10 @@ public:
 };
 
 
+typedef enum { easy, mid, hard, none } difficulties;
 
+
+// FUNCTION PROTOTYPES
 
 void setting_btn_clicked(int x, int y, DifficultyBtn &easy, DifficultyBtn &mid, DifficultyBtn &hard, difficulties &difficulty_setting);
 
@@ -59,6 +60,9 @@ int* get_neighbours(int box_number, int grid_size);
 
 void handle_grid_click(int button_clicked, int *revealed, int* box_values, int grid_size);
 
+
+
+// MAIN AND GAME FUNCTIONS
 int main() {
 
 	int box_size[3] = {65, 52, 42};
@@ -72,9 +76,10 @@ int main() {
 	int mouse_y = 0;
 	int button_clicked = 0;
 	int* box_values = nullptr;
-
 	int* revealed = nullptr;
 
+
+	// SFML LIBRARY
 	sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({ WIDTH, HEIGHT }), "minesweeper");
 	window.setFramerateLimit(69);
 
@@ -86,26 +91,23 @@ int main() {
 	sf::Text heading(font, "MINESWEEPER", 44);
 	heading.setPosition({ WIDTH / 2.0f - (44*11/2), 100});
 
-
-	sf::Text newthings(font, "guess who's back", 44);
-	newthings.setPosition({ WIDTH / 2.0f - (44 * 15 / 2), 121 });
-
 	difficulties difficulty_setting = none;
 
-	// INSTANTIATE THE BUTTONS
 
+	// INSTANTIATE THE BUTTONS
 	DifficultyBtn hardBtn("HARD", font, 600.0f);
 	DifficultyBtn midBtn("MID", font, 450.0f);
 	DifficultyBtn easyBtn("EASY", font, 300.0f);
 	
 
+	// MAIN GAME WINDOW
 	while (window.isOpen()) {
-		while (std::optional event = window.pollEvent()) {
+		while (std::optional event = window.pollEvent()) {				// cross is clicked
 			if (event->is<sf::Event::Closed>()) {
 				window.close();
 			}
 
-			else if (auto* keypressed = event->getIf<sf::Event::KeyPressed>()) {
+			else if (auto* keypressed = event->getIf<sf::Event::KeyPressed>()) {			// escape pressed
 				if (keypressed->scancode == sf::Keyboard::Scancode::Escape) {
 					window.close();
 				}
@@ -119,14 +121,11 @@ int main() {
 					if (difficulty_setting == none) {
 						setting_btn_clicked(mouse_x, mouse_y, easyBtn, midBtn, hardBtn, difficulty_setting);
 
-						if (difficulty_setting != none) {
+						if (difficulty_setting != none) {		// difficulty not set yet
 							int total_tiles = grid_size[difficulty_setting] * grid_size[difficulty_setting];
 							bomb_indices = get_bomb_indices(num_bombs[difficulty_setting], grid_size[difficulty_setting]);
 							
-							//int total_tiles = grid_size[difficulty_setting] * grid_size[difficulty_setting];
 							revealed = new int[total_tiles]();
-							// getting box values in the old way that i have been doing...
-
 
 							box_values = (int*)malloc((grid_size[difficulty_setting] * grid_size[difficulty_setting]) * sizeof(int));
 
@@ -142,10 +141,9 @@ int main() {
 								}
 							}
 						}
-						
-						
 					}
-					else {
+
+					else {		// difficulty set, now render the grid
 
 						int p = padding[0];
 						int ox = offset_x[difficulty_setting];
@@ -157,24 +155,14 @@ int main() {
 
 						if (col >= 1 && col <= grid_size[difficulty_setting] && row >= 1 && row <= grid_size[difficulty_setting]) {
 							button_clicked = (row - 1) * grid_size[difficulty_setting] + col;
-							//revealed[button_clicked - 1] = 1;
-							//std::cout << "box number, " << button_clicked << " was clicked" << std::endl;
 						}
 
 						handle_grid_click(button_clicked, revealed, box_values, grid_size[difficulty_setting]);
 
+
+						// to reset the mouse clicked coordinates
 						mouse_x = -1;
 						mouse_y = -1;
-
-
-
-
-						// so revealed stores the boxes that havebeen opened in its memory:-
-						// 
-						//for (int i = 0; i < (grid_size[difficulty_setting] * grid_size[difficulty_setting]); i++) {
-						//	std::cout << revealed[i] << " ";
-						//}
-
 					}
 				}
 			}
@@ -182,12 +170,10 @@ int main() {
 	
 
 		// render object
-
 		window.clear(sf::Color::Magenta);
 
 		// draw objects
-
-		if (difficulty_setting == none) {
+		if (difficulty_setting == none) {			// when difficulty not set, draw difficulty setting buttons
 			window.draw(easyBtn);
 			window.draw(midBtn);
 			window.draw(hardBtn);
@@ -199,7 +185,7 @@ int main() {
 			window.draw(heading);
 		}
 
-		else {
+		else {				// when difficulty is set, draw the game grid
 			draw_grid(window, font, difficulty_setting, box_size, grid_size, num_bombs, bomb_indices, button_clicked, revealed, box_values);
 		}
 		
@@ -210,77 +196,10 @@ int main() {
 	return 0;
 }
 
-/*
-void handle_grid_click(int button_clicked, int* revealed, int* box_values, int grid_size) {
-	// individual button clicks reach here, now you have the revealed arr
-
-
-	//for (int i = 0; i < grid_size; i++) {
-	//	for (int j = 0; j < grid_size; j++) {
-	//		std::cout << revealed[grid_size * i + j] << " ";
-	//	}
-	//	std::cout << std::endl;
-	//}
-
-	std::cout << "box number: " << button_clicked << " box value: " << box_values[button_clicked - 1] << std::endl;
-
-	if (revealed[button_clicked - 1] == 1) {
-		std::cout << "box already clicked" << std::endl;
-		return;
-	}
-
-
-	else {
-		if (box_values[button_clicked - 1] == -1) {
-			revealed[button_clicked - 1] = 1;
-			std::cout << "you clicked on a bomb, GAME OVER..." << std::endl;
-			return;
-		}
-
-		else if (box_values[button_clicked - 1] != 0) {
-			revealed[button_clicked - 1] = 1;
-			return;
-		}
-
-		else if (box_values[button_clicked - 1] == 0) {
-			revealed[button_clicked - 1] = 1;
-
-			// when zero is clicked, get its neighbours, then for each of the neighbours, check if they are a number, if they are, simply reveal them
-			// if they are another zero, then call recursion, a zero HAS TO HAVE numbers in its surroundings, it CANNOT have a bomb in its surroudings.
-			int neighbour_counter = 0;
-			int* neighbours = get_neighbours(button_clicked, grid_size);
-			int curr_box_num = neighbours[neighbour_counter];
-
-			while (curr_box_num != -1) {
-
-				handle_grid_click(curr_box_num, revealed, box_values, grid_size);
-
-				//revealed[curr_box_num - 1] = 1;
-				//if (box_values[curr_box_num - 1] == 0) {
-				//	handle_grid_click(curr_box_num, revealed, box_values, grid_size);
-				//}
-				neighbour_counter++;
-
-				//return;
-			}
-			//return;
-		}
-
-		else {
-			std::cout << "handle_grid_click encountered an unaccounted for case..." << std::endl;
-		}
-	}
-
-	
-
-}
-*/
-
 
 void handle_grid_click(int button_clicked, int* revealed, int* box_values, int grid_size) {
 
-	std::cout << "box number: " << button_clicked << " box value: " << box_values[button_clicked - 1] << std::endl;
-
+	//std::cout << "box number: " << button_clicked << " box value: " << box_values[button_clicked - 1] << std::endl;
 
 	if (button_clicked > (grid_size * grid_size) || button_clicked < 1) {
 		// tile outside of the boundary is clicked
@@ -288,7 +207,8 @@ void handle_grid_click(int button_clicked, int* revealed, int* box_values, int g
 	}
 
 	if (box_values[button_clicked - 1] == -1 || revealed[button_clicked - 1] == 1) {
-		return;			// for now account for both the cases, when tile opened or is a bomb in the same case
+		// for now account for both the cases, when tile opened or is a bomb in the same case
+		return;			
 	}
 
 	if (box_values[button_clicked - 1] >= 1 && box_values[button_clicked - 1] <= 8) {
@@ -301,23 +221,14 @@ void handle_grid_click(int button_clicked, int* revealed, int* box_values, int g
 
 		int neighbour_counter = 0;
 		int* neighbours = get_neighbours(button_clicked, grid_size);
-		//int curr_box_num = neighbours[neighbour_counter];
 
 		while (neighbours[neighbour_counter] != -1) {
 
 			handle_grid_click(neighbours[neighbour_counter], revealed, box_values, grid_size);
 
-			//revealed[curr_box_num - 1] = 1;
-			//if (box_values[curr_box_num - 1] == 0) {
-			//	handle_grid_click(curr_box_num, revealed, box_values, grid_size);
-			//}
 			neighbour_counter++;
-
-			//return;
 		}
-		//return;
 	}
-	
 }
 
 int* get_neighbours(int box_number, int grid_size) {
@@ -425,15 +336,14 @@ void setting_btn_clicked(int x, int y, DifficultyBtn &easy, DifficultyBtn &mid, 
 
 	if (easy.getGlobalBounds().contains(mousePos)) {
 		difficulty_setting = difficulties::easy;
-		// start game in easy mode
 	}
+
 	else if (mid.getGlobalBounds().contains(mousePos)) {
 		difficulty_setting = difficulties::mid;
-		// start game in medium mode
 	}
+
 	else if (hard.getGlobalBounds().contains(mousePos)) {
 		difficulty_setting = difficulties::hard;
-		// start game in hard mode
 	}
 }
 
@@ -441,42 +351,31 @@ void setting_btn_clicked(int x, int y, DifficultyBtn &easy, DifficultyBtn &mid, 
 void draw_grid(sf::RenderWindow& window, sf::Font& font, difficulties difficulty_setting,
 	int box_size[], int grid_size[], int num_bombs[], int bomb_indices[], int button_clicked, int *revealed, int box_values[]) {				// pass by value will also work for the three matrices
 	
-	
 	int padding[3] = { 3, 3, 3 };
 	int offset_x[3] = { 7, 4, 9 };
 	int offset_y[3] = { 126, 120, 136 };
-
-	//int* box_values = (int*)malloc((grid_size[difficulty_setting] * grid_size[difficulty_setting]) * sizeof(int));
 
 	for (int i = 1; i <= grid_size[difficulty_setting]; i++) {
 		for (int j = 1; j <= grid_size[difficulty_setting]; j++) {
 
 			int box_number = (i - 1) * grid_size[difficulty_setting] + j;
-			//if (box_values != nullptr) {
-			//	box_values[box_number - 1] = get_box_number(bomb_indices, box_number, grid_size[difficulty_setting], num_bombs[difficulty_setting]);
-			//}
-			//else {
-			//	std::cout << "Error in inserting values to boxes..." << std::endl;
-			//}
+
 			sf::RectangleShape rect(sf::Vector2f(box_size[difficulty_setting], box_size[difficulty_setting]));
 
 			rect.setPosition({ (float)box_size[difficulty_setting] * (j - 1) + padding[difficulty_setting] * j + offset_x[difficulty_setting],
 							   (float)box_size[difficulty_setting] * (i - 1) + padding[difficulty_setting] * i + offset_y[difficulty_setting] });
 
-
-			// if revealed at this tile number is 1, then color it something else...
-
 			if (revealed[box_number - 1]) {
 				rect.setFillColor(sf::Color::Black);
-				//std::cout << "box number: " << box_number << " box value: " << box_values[box_number - 1] << std::endl;
 			}
 			
 			window.draw(rect);
-
-			// we could send in the mouse_x and mouse_y to this function itself and handle it within this function, within this loop.
 		}
 	}
 }
+
+
+// MISCELLANEOUS FUNCTION, (HELPERS)
 
 int get_value(int bomb_indices[], int neighbours[], int num_bomb, int num_of_neighbours) {
 	int value = 0;
@@ -487,6 +386,7 @@ int get_value(int bomb_indices[], int neighbours[], int num_bomb, int num_of_nei
 	}
 	return value;
 }
+
 
 int get_box_number(int bomb_indices[], int box_number, int rows, int num_bomb) {
 	int value = 0;
